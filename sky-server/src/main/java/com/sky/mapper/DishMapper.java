9ac77cf54053fa1a6a435888041c51f39e3bbbe1
yinmaoxu <3181160162@qq.com>
2025-03-14 11:02:39 +0800

    菜品管理： 新增菜品

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,6 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -11,4 +14,7 @@ public interface DishMapper {
 
     @Select("SELECT * FROM dish WHERE category_id = #{id}")
     List<Dish> findByCate(long id);
+
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dish);
 }
